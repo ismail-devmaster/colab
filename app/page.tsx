0db890f62e4f13c2dda0d29a6821b9bc963d4d66
xCyberpunkx <peeplil6666@gmail.com>
2024-11-03 22:47:30 +0100

    Added Doc To  codebase & hotFixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/app/components/ui/card";
 import { Calendar, Users, UserRound, Activity, Clock, Home } from "lucide-react";
 
 export default function HomePage() {
